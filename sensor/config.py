b9c314d20f29a321e5827711fba53a4baf412bfc
skprasad117 <skprasad117@gmail.com>
2022-12-03 15:13:39 +0530

    updated

--- sensor/config.py
+++ sensor/config.py
@@ -1,11 +1,13 @@
 import pymongo
 import pandas as pd
 import json
+from dataclasses import dataclass
+import os
+@dataclass
+class EnvironmentVariable:
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+    aws_access_key_id:str = os.getenv("AWS_ACCESS_KEY_ID")
+    aws_access_secret_key:str = os.getenv("AWS_SECRET_ACCESS_KEY")
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb+srv://sanjay:password@cluster0.m69g2.mongodb.net/?retryWrites=true&w=majority")
-
-DATA_FILE_PATH = "aps_failure_training_set1.csv"
-# Database Name
-DATABASE_NAME = "aps"
-COLLECTION_NAME = "sensor"
+env_var = EnvironmentVariable()
+mongo_client = pymongo.MongoClient(env_var.mongo_db_url)
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -1,4 +1,22 @@
 import pandas as pd 
-def get_collection_as_dataframe()->pd.DataFrame:
-    pass
-    
+from sensor.config import mongo_client
+from sensor.logger import logging
+from sensor.config import SensorException
+
+def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
+    """
+    This function return collection as dataframe
+    database_name : database name
+    """
+
+    try:
+        logging.info(f"reading data from database")
+        df = DataFrame(list(mongo_client[database_name][collection_name].find()))
+        logging.info(f"Found Columns : {df.columns}")
+        if "_id" in df.columns:
+            logging.info(d"Dropping Column : _id")
+            df = df.drop("_id",axis = 1)
+        logging.info(f"Row and Columns in df : {df.shape}")
+    return df
+    except Exception as e:
+        raise SensorException(e,sys)
