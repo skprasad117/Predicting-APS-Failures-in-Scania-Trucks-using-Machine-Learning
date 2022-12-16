fa28e9888829b06c4eeab3604ef9269b630c00e1
skprasad117 <skprasad117@gmail.com>
2022-12-17 01:04:45 +0530

    update

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from sensor.logger import logging
 from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
 import sys,os
 
 if __name__ == "__main__":
-    try:...
+    try:
+        get_collection_as_dataframe(database_name="aps",collection_name="sensor")
     except Exception as e:
         print(e)
--- sensor/utils/__init__.py
+++ sensor/utils/__init__.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 from sensor.config import mongo_client
 from sensor.logger import logging
-from sensor.config import SensorException
+from sensor.exception import SensorException
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
@@ -14,9 +14,9 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         df = DataFrame(list(mongo_client[database_name][collection_name].find()))
         logging.info(f"Found Columns : {df.columns}")
         if "_id" in df.columns:
-            logging.info(d"Dropping Column : _id")
+            logging.info(f"Dropping Column : _id")
             df = df.drop("_id",axis = 1)
         logging.info(f"Row and Columns in df : {df.shape}")
-    return df
+        return df
     except Exception as e:
         raise SensorException(e,sys)
