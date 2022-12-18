32b76847d0a194746e937a90f0fa9a2215e14187
skprasad117 <skprasad117@gmail.com>
2022-12-18 23:18:37 +0530

    update

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity import config_entity
 
 if __name__ == "__main__":
     try:
-        get_collection_as_dataframe(database_name="aps",collection_name="sensor")
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
