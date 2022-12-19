20df628181f89bf62da19f5ef9e0cdd9ee468578
skprasad117 <skprasad117@gmail.com>
2022-12-19 16:21:19 +0530

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from sensor.entity import config_entity
 if __name__ == "__main__":
     try:
         training_pipeline_config = config_entity.TrainingPipelineConfig()
-        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
         print(data_ingestion_config.to_dict())
     except Exception as e:
         print(e)
