92936e13701361ae80138e333e1642a09ebd5ae6
sanjay prasad <skprasad117>
2022-12-21 03:15:18 +0530

    data validation completed

--- sensor/utils.py
+++ sensor/utils.py
@@ -3,6 +3,7 @@ from sensor.config import mongo_client
 from sensor.logger import logging
 from sensor.exception import SensorException
 import sys,os
+import yaml
 
 def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
@@ -21,3 +22,24 @@ def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataF
         return df
     except Exception as e:
         raise SensorException(e,sys)
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e,sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]= df[column].astype('float')
+        return df
+    except Exception as e:
+        raise SensorException(e,sys)        
+
+    
