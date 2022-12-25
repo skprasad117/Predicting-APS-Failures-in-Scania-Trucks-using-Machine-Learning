736249ae739f60499a1b0e5462c6340ee988d255
sanjay prasad <skprasad117>
2022-12-25 14:47:25 +0530

    update

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 from sensor import utils
 import os,sys
+from sensor.config import TARGET_COLUMN
 
 
 class DataValidation:
@@ -123,7 +124,7 @@ class DataValidation:
             logging.info(f"Drop null values column from test df")
             test_df= self.drop_missing_values_columns(df=test_df, report_key_name="missing_values_within_test_dataset")
 
-            exclude_columns = ["class"]
+            exclude_columns = [TARGET_COLUMN]
             base_df = utils.convert_columns_float(df=base_df, exclude_columns=exclude_columns)
             train_df = utils.convert_columns_float(df=train_df, exclude_columns=exclude_columns)
             test_df = utils.convert_columns_float(df=test_df, exclude_columns=exclude_columns)
